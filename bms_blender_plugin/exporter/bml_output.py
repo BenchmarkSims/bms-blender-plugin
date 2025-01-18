cf42163a18ab33e7ededbe7e1e85a41b36484fe6
tumbler <tumbler@benchmarksims.org>
2025-01-18 17:45:28 +0100

    Bounding box & scaling fix
    - bounding box uses min/max coordinates of mesh entities only and excludes eg camera
    - radius in parant.dat is correctly scaled
    - default nr of switches and dofs set to zero in parant.dat to prevent editor crashes

--- bms_blender_plugin/exporter/bml_output.py
+++ bms_blender_plugin/exporter/bml_output.py
@@ -31,18 +31,14 @@ def export_bml(context, lods, file_directory, file_prefix, export_settings: Expo
     start_time = datetime.datetime.now()
     print(f"Starting BML export at {start_time}\n")
 
-    # determine whether the current scene is set to imperial units, otherwise we have to scale our objects
-    scale_factor = 1
+    # blender uses meters as base unit, BMS works in feet
+    # note that unit system does not scale the model, it just changes the dimension units
+    scale_factor = 3.28084
 
-    if bpy.context.scene.unit_settings.system == "METRIC":
-        if bpy.context.scene.unit_settings.length_unit == "METERS":
-            scale_factor = 3.28084
-        elif bpy.context.scene.unit_settings.length_unit == "CENTIMETERS":
-            scale_factor = 3.28084 * 100
-        elif bpy.context.scene.unit_settings.length_unit == "MILLIMETERS":
-            scale_factor = 3.28084 * 1000
-    elif bpy.context.scene.unit_settings.length_unit == "INCHES":
-        scale_factor = 0.0833333
+    # apply the scale factor as set in scene->units->unit scale
+    # this factor does not scale the model, it scales the dimension units
+    # to get this reflected in the model, apply this on scale factor
+    scale_factor *= bpy.context.scene.unit_settings.scale_length
 
     print(f"unit scaling factor: {scale_factor}")
 
@@ -52,8 +48,18 @@ def export_bml(context, lods, file_directory, file_prefix, export_settings: Expo
     # try to find the bounding box - we take the first one we get
     for obj in context.scene.objects:
         if get_bml_type(obj) == BlenderNodeType.BBOX:
-            bounding_box_1_coords = obj.matrix_world @ obj.data.vertices[0].co
-            bounding_box_2_coords = obj.matrix_world @ obj.data.vertices[5].co
+            max_x, min_x, max_y, min_y, max_z, min_z = (0,)*6
+            # find the min and max coordinates of the 8 vertices
+            for corner in obj.data.vertices:
+                max_x = max(corner.co.x, max_x)
+                min_x = min(corner.co.x, min_x)
+                max_y = max(corner.co.y, max_y)
+                min_y = min(corner.co.y, min_y)
+                max_z = max(corner.co.z, max_z)
+                min_z = min(corner.co.z, min_z)
+
+            bounding_box_1_coords = Vector((max_x, max_y, max_z))
+            bounding_box_2_coords = Vector((min_x, min_y, min_z))
 
             bounding_box_1_coords *= scale_factor
             bounding_box_2_coords *= scale_factor
@@ -90,6 +96,7 @@ def export_bml(context, lods, file_directory, file_prefix, export_settings: Expo
             file_prefix,
             bounding_box_1_coords,
             bounding_box_2_coords,
+            scale_factor,
             number_of_texture_sets,
             get_slots(context.scene),
             lods
