2a7d693948ad8c873ceb111237e1ba5ce5c39ecc
lajouvet <jouvetlaurent@gmail.com>
2025-04-03 10:34:05 +0200

    Use to_bms_coords

--- bms_blender_plugin/common/hotspot.py
+++ bms_blender_plugin/common/hotspot.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from mathutils import Vector
+from bms_blender_plugin.common.util import to_bms_coords
 
 
 class MouseButton(int, Enum):
@@ -42,21 +44,19 @@ class Hotspot:
         self.mouse_button = mouse_button
         self.button_type = button_type
 
-    def __str__(self):
-        """"Format according to 3dButtons.dat.
-        BMS should have no problems reading different amounts of spaces, but it looks prettier..."""
-        # Perform coordinate transformation: Blender (X,Y,Z) to BMS (-Z,-X,+Y)
-        bms_x = -self.z
-        bms_y = -self.x
-        bms_z = self.y
-        
-        output = f"{self.callback_id.ljust(29 ,' ')}"
-        output += "{:{w}.{p}f}{:{w}.{p}f}{:{w}.{p}f}".format(
-            round(bms_x, 6), round(bms_y, 6), round(bms_z, 6), w=12, p=6
-        )
-        output += f"{' ':<4}{self.size}{' ':<4}{self.sound_id}{' ':<4}{self.mouse_button}"
+def __str__(self):
+    """"Format according to 3dButtons.dat.
+    BMS should have no problems reading different amounts of spaces, but it looks prettier..."""
+    # Use consistent coordinate transformation utility
+    bms_coords = to_bms_coords(Vector((self.x, self.y, self.z)))
+    
+    output = f"{self.callback_id.ljust(29 ,' ')}"
+    output += "{:{w}.{p}f}{:{w}.{p}f}{:{w}.{p}f}".format(
+        round(bms_coords.x, 6), round(bms_coords.y, 6), round(bms_coords.z, 6), w=12, p=6
+    )
+    output += f"{' ':<4}{self.size}{' ':<4}{self.sound_id}{' ':<4}{self.mouse_button}"
 
-        if self.button_type != ButtonType.SWITCH:
-            output += f"{' ':<4}{self.button_type}"
+    if self.button_type != ButtonType.SWITCH:
+        output += f"{' ':<4}{self.button_type}"
 
-        return output
+    return output
