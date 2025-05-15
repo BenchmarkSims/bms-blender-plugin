4fc01c19f8faeee0648d1c5cbcbd3ccc98238d77
Yaan <yaan.dalzell@gmail.com>
2025-05-15 16:36:06 +1000

    #25. Fixed hotspot __str__ function to return correctly formatted string for 3dButtons.dat

--- bms_blender_plugin/common/hotspot.py
+++ bms_blender_plugin/common/hotspot.py
@@ -44,19 +44,19 @@ class Hotspot:
         self.mouse_button = mouse_button
         self.button_type = button_type
 
-def __str__(self):
-    """"Format according to 3dButtons.dat.
-    BMS should have no problems reading different amounts of spaces, but it looks prettier..."""
-    # Use consistent coordinate transformation utility
-    bms_coords = to_bms_coords(Vector((self.x, self.y, self.z)))
-    
-    output = f"{self.callback_id.ljust(29 ,' ')}"
-    output += "{:{w}.{p}f}{:{w}.{p}f}{:{w}.{p}f}".format(
-        round(bms_coords.x, 6), round(bms_coords.y, 6), round(bms_coords.z, 6), w=12, p=6
-    )
-    output += f"{' ':<4}{self.size}{' ':<4}{self.sound_id}{' ':<4}{self.mouse_button}"
+    def __str__(self):
+        """"Format according to 3dButtons.dat.
+        BMS should have no problems reading different amounts of spaces, but it looks prettier..."""
+        # Use consistent coordinate transformation utility
+        bms_coords = to_bms_coords(Vector((self.x, self.y, self.z)))
 
-    if self.button_type != ButtonType.SWITCH:
-        output += f"{' ':<4}{self.button_type}"
+        output = f"{self.callback_id.ljust(29 ,' ')}"
+        output += "{:{w}.{p}f}{:{w}.{p}f}{:{w}.{p}f}".format(
+            round(bms_coords.x, 6), round(bms_coords.y, 6), round(bms_coords.z, 6), w=12, p=6
+        )
+        output += f"{' ':<4}{self.size}{' ':<4}{self.sound_id}{' ':<4}{self.mouse_button}"
 
-    return output
+        if self.button_type != ButtonType.SWITCH:
+            output += f"{' ':<4}{self.button_type}"
+
+        return output
